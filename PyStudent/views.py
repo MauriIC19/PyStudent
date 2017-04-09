316a787430ae1afa19e897a714ee4affcef585f7
MauriIC19 <sasuke_arti@hotmail.com>
2017-04-09 11:49:26 -0500

    Estructura básica del proyecto

--- PyStudent/views.py
+++ PyStudent/views.py
@@ -1,8 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
 
 def index(request):
     template = loader.get_template('index.html')
-    name = {"name": "Mauricio"}
-    return HttpResponse(template.render(name))
+    return HttpResponse(template.render())
