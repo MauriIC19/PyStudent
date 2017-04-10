687849758a6c2f24d44a06a8785f30cc5358fe98
MauriIC19 <sasuke_arti@hotmail.com>
2017-04-09 20:04:26 -0500

    Ejemplo de navegación entre vistas

--- PyStudent/views.py
+++ PyStudent/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def registro(request):
+    template = loader.get_template('registro.html')
+    return HttpResponse(template.render())
