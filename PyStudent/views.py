35b9ba4d74ad06bee59f70bf7b710611e5af8f3d
MauriIC19 <sasuke_arti@hotmail.com>
2017-04-13 17:56:06 -0500

    Insert desde formulario mediante POST a la BD

--- PyStudent/views.py
+++ PyStudent/views.py
@@ -2,20 +2,36 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from PyStudent.models import Alumno
+from django.views.decorators.csrf import *
 
 def index(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render(request, 'index.html')
 
+#@csrf_exempt
 def registro(request):
-    template = loader.get_template('registro.html')
-    return HttpResponse(template.render())
+
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        correo = request.POST.get('correo')
+
+        al = Alumno(name=nombre, mail=correo)
+        al.save()
+
+        context = {
+            'aviso':'Alumno agregado'
+        }
+
+        return render(request, 'registro.html', context)
+
+    else:
+        return render(request, 'registro.html')
 
 def datos(request):
     """Consulta a la base de datos que obtiene todos los datos de esa tabla
        Si quisieramos obtener los datos de un usuario en específico, se usa el
        siguiente comando: Alumno.objects.get(var=algo)"""
-       
+
     al = Alumno.objects.all()
     alumnos = {}
 
@@ -24,5 +40,4 @@ def datos(request):
 
     context ={'a': alumnos}
 
-    template = loader.get_template('datos.html')
-    return HttpResponse(template.render(context))
+    return render(request, 'datos.html', context)
