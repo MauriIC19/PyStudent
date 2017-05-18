3838eec5e40b0de91265bfb741a38b15ecd76775
MauriIC19 <sasuke_arti@hotmail.com>
2017-05-18 08:14:59 -0500

    Pequeños cambios.

--- PyStudent/admin.py
+++ PyStudent/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Alumno, Estado, Puntaje, Palabras
 
 # Register your models here.
+MyModels = [Alumno, Estado, Puntaje, Palabras]
+admin.site.register(MyModels)
